18bf05f362e79421634c18f58d12996b85280a86
Steve Ellis <email@steveell.is>
2013-01-13 02:22:14 -0500

    Don't expose unused public methods.

--- lib/better_receive/mock.rb
+++ lib/better_receive/mock.rb
@@ -1,28 +1,35 @@
 module BetterReceive
   class Mock
-    attr_reader :expectation, :object
 
     def initialize(object)
       @object = object
     end
 
     def verify(selector, options, &block)
-      object.should respond_to(selector)
+      better_respond_to(selector)
 
       mock_method(selector, options, &block)
     end
 
     private
 
-    def mock_method(selector, options, &block)
-      mock_proxy = object.send(:__mock_proxy)
+    attr_reader :object
 
+    def better_respond_to(selector)
+      object.should respond_to(selector)
+    end
+
+    def mock_method(selector, options, &block)
       location = options[:expected_from] || caller(1)[2]
-      @expectation = mock_proxy.add_message_expectation(location, selector, options, &block)
+      mock_proxy.add_message_expectation(location, selector, options, &block)
     end
 
     def respond_to(selector)
       RSpec::Matchers::BuiltIn::RespondTo.new(selector)
+    end
+
+    def mock_proxy
+      @mock_proxy ||= object.send(:__mock_proxy)
     end
 
   end
