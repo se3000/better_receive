2d8961aab4e7a322576fdfc691e4260bf4984805
Steve Ellis <email@steveell.is>
2013-01-26 13:42:08 -0500

    Reduce mock logic to only 2 paths.

--- lib/better_receive/mock.rb
+++ lib/better_receive/mock.rb
@@ -1,55 +1,56 @@
 module BetterReceive
   class Mock
 
-    def initialize(object)
-      @object = object
+    def initialize(subject)
+      @subject = subject
     end
 
     def responds_to_and_receives(selector, options, &block)
-      better_respond_to(selector)
-
-      mock_method(selector, options, &block)
+      if subject.is_a?(RSpec::Mocks::AnyInstance::Recorder)
+        any_instance_better_receive(selector, options, &block)
+      else
+        subject.should respond_to selector
+        mock_subject_method(selector, options, &block)
+      end
     end
+
 
     private
 
-    attr_reader :object
-
-    def better_respond_to(selector)
-      if any_instance?
-        any_instance_should_respond_to selector
-      else
-        object.should respond_to(selector)
-      end
-    end
-
-    def any_instance_should_respond_to(selector)
-      klass = object.instance_variable_get(:@klass)
-      unless klass.method_defined?(selector)
-        raise "Expected instances of #{klass.name} to respond to :#{selector}"
-      end
-    end
+    attr_reader :subject
 
     def respond_to(selector)
       RSpec::Matchers::BuiltIn::RespondTo.new(selector)
     end
 
-    def mock_method(selector, options, &block)
-      if any_instance?
-        any_instance_should_receive selector, &block
-      else
-        location = options[:expected_from] || caller(1)[2]
-        mock_proxy.add_message_expectation(location, selector, options, &block)
+    def mock_subject_method(selector, options, &block)
+      location = options[:expected_from] || caller(1)[2]
+      subject_mock_proxy.add_message_expectation(location, selector, options, &block)
+    end
+
+    def subject_mock_proxy
+      @mock_proxy ||= subject.send(:__mock_proxy)
+    end
+
+    def any_instance_better_receive(selector, options, &block)
+      any_instance_should_respond_to selector
+      any_instance_should_receive selector, &block
+    end
+
+    def any_instance_should_respond_to(selector)
+      klass = subject.instance_variable_get(:@klass)
+      unless klass.method_defined?(selector)
+        raise "Expected instances of #{klass.name} to respond to :#{selector}"
       end
     end
 
     def any_instance_should_receive(selector, &block)
-      RSpec::Mocks::space.add(object)
+      RSpec::Mocks::space.add(subject)
 
-      object.instance_variable_set(:@expectation_set, true)
-      object.send(:observe!, selector)
+      subject.instance_variable_set(:@expectation_set, true)
+      subject.send(:observe!, selector)
 
-      object.message_chains.add(selector, expectation_chain(selector, &block))
+      subject.message_chains.add(selector, expectation_chain(selector, &block))
     end
 
     def expectation_chain(*args)
@@ -58,14 +59,6 @@ module BetterReceive
       else
         RSpec::Mocks::AnyInstance::ExpectationChain.new(*args)
       end
-    end
-
-    def any_instance?
-      object.is_a?(RSpec::Mocks::AnyInstance::Recorder)
-    end
-
-    def mock_proxy
-      @mock_proxy ||= object.send(:__mock_proxy)
     end
 
   end
